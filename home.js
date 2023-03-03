52b0e124208c3ee7f63c33ad1198fa02394d3cef
Andrew Ghaly <ghalyandrew@csu.fullerton.edu>
2023-03-02 19:29:00 -0800

    Initial commit

---
+++ home.js
@@ -0,0 +1,16 @@
+const imageContainerElement = document.querySelector(".image-container");
+const buttonElement = document.querySelector(".btn");
+
+buttonElement.addEventListener("click", ()=>{
+    console.log("Clicked");
+    imageNum = 10;
+    addNewImages();
+});
+
+function addNewImages() {
+    for (let index = 0; index < imageNum; index++) {
+        const newImage = document.createElement("img");
+        newImage.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
+        imageContainerElement.appendChild(newImage);
+    }
+}
